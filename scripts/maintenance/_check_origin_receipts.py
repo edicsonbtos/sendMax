659185010ff7d81418dda0816f9914340b92fe4c
edicsonbtos <110593@proton.me>
2026-02-13 16:17:01 -0400

    chore: limpiar scripts maintenance y temp files del repo, agregar a gitignore

--- scripts/maintenance/_check_origin_receipts.py
+++
@@ -1,7 +0,0 @@
-﻿import os, psycopg
-url = os.environ["DATABASE_URL"]
-with psycopg.connect(url) as conn:
-    with conn.cursor() as cur:
-        cur.execute("SELECT day, origin_country, fiat_currency, amount_fiat FROM origin_receipts_daily ORDER BY day, origin_country;")
-        for r in cur.fetchall():
-            print(r)
--- scripts/maintenance/_last_order.py
+++
@@ -1,6 +0,0 @@
-﻿import os, psycopg
-url = os.environ["DATABASE_URL"]
-with psycopg.connect(url) as c:
-    with c.cursor() as cur:
-        cur.execute("select public_id,status,created_at from orders order by created_at desc limit 1;")
-        print(cur.fetchone())
